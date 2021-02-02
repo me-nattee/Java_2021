e3af899e2a81b72c98f80d70abb2c4e2c526b9bf
Natalia Shakh <me.nattee@gmail.com>
2021-02-02 15:22:53 +0300

    Поправила импорты

--- sandbox/src/test/java/ru/autoqa/Java2021/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/autoqa/Java2021/sandbox/PointTests.java
@@ -1,7 +1,8 @@
 package ru.autoqa.Java2021.sandbox;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
 
 public class PointTests {
     @Test
@@ -9,20 +10,20 @@ public class PointTests {
     public void testPoint(){
         Point p1 = new Point(5,7);
         Point p2 = new Point(2,3);
-        Assert.assertEquals(p1.distance(p2), 5.0,0);
+        Assert.assertEquals(p1.distance(p2), 5.0);
     }
 
     @Test
     public void testPoint2(){
         Point p1 = new Point(5,7);
         Point p2 = new Point(2,7);
-        Assert.assertEquals(p1.distance(p2), 5.0,0);
+        Assert.assertEquals(p1.distance(p2), 2.0);
     }
 
     @Test
     public void testPoint3(){
         Point p1 = new Point(5,7);
         Point p2 = new Point(5,7);
-        Assert.assertEquals(p1.distance(p2), 2.0,3);
+        Assert.assertEquals(p1.distance(p2), 2.0);
     }
 }
