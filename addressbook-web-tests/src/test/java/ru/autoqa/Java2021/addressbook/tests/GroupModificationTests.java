2f7001e9676b82aa406bba43e0742a8621408b9b
Natalia Shakh <me.nattee@gmail.com>
2021-02-16 16:48:37 +0300

    Лекция 3.10. Проверка и обеспечение выполнения предусловий

--- addressbook-web-tests/src/test/java/ru/autoqa/Java2021/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/autoqa/Java2021/addressbook/tests/GroupModificationTests.java
@@ -8,10 +8,10 @@ public class GroupModificationTests extends TestBase {
     @Test
     public void testGroupModification() {
         app.getNavigationHelper().gotoGroupPage();
+        if (! app.getGroupHelper().isThereAGroup()){
+            app.getGroupHelper().createGroup(new GroupData("Test1", null, null));
+        }
         app.getGroupHelper().selectGroup();
-        app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("Test1", null,null));
-        app.getGroupHelper().submitGroupModification();
-        app.getGroupHelper().returnToGroupPage();
+        app.getGroupHelper().createGroup(new GroupData("Test1", null, null));
     }
 }
