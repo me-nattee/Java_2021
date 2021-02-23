ee6e4d5c427081eff8884c08d298ca8747f89202
Natalia Shakh <me.nattee@gmail.com>
2021-02-23 23:38:41 +0300

    Лекция 4.8. Поиск максимального элемента в коллекции в группах

--- addressbook-web-tests/src/test/java/ru/autoqa/Java2021/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/autoqa/Java2021/addressbook/tests/GroupCreationTests.java
@@ -4,17 +4,30 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.autoqa.Java2021.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() throws Exception {
-    app.getNavigationHelper().gotoGroupPage();
-    List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().createGroup(new GroupData("Test1", null, null));
-    List<GroupData> after = app.getGroupHelper().getGroupList();
-    Assert.assertEquals(after.size(), before.size() + 1);
-  }
+    @Test
+    public void testGroupCreation() throws Exception {
+        app.getNavigationHelper().gotoGroupPage();
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        GroupData group = new GroupData("Test1", null, null);
+        app.getGroupHelper().createGroup(group);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size() + 1);
+
+
+        int max = 0;
+        for (GroupData g : after) {
+            if (g.getId() > max) {
+                max = g.getId();
+            }
+        }
+        group.setId(max);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+    }
 
 }
