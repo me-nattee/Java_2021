8d50a3a2e455bbfefe551286afe55dcbab2dd3e6
Natalia Shakh <me.nattee@gmail.com>
2021-02-26 20:49:42 +0300

    Лекция 5.3. Улучшение названий методов в группах

--- addressbook-web-tests/src/test/java/ru/autoqa/Java2021/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/autoqa/Java2021/addressbook/tests/ContactCreationTests.java
@@ -5,18 +5,17 @@ import org.testng.annotations.Test;
 import ru.autoqa.Java2021.addressbook.model.ContactData;
 
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 
 public class ContactCreationTests extends TestBase {
 
     @Test
     public void testCreationNewContact() throws Exception {
-        app.getNavigationHelper().returnToHomePage();
+        app.goTo().returnToHomePage();
         List<ContactData> before = app.getContactHelper().getContactList();
         ContactData contact = new ContactData("Mary", "Brown", "mar", "Jn", "Petrozavodsk", "335999", "8923294455","dfdfn@gmail.com", "ssstr.ru", "6", "February", "1990", "-", "Test1","hi!");
         app.getContactHelper().createContact(contact);
-        app.getNavigationHelper().returnToHomePage();
+        app.goTo().returnToHomePage();
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
--- addressbook-web-tests/src/test/java/ru/autoqa/Java2021/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/autoqa/Java2021/addressbook/tests/ContactModificationTests.java
@@ -3,10 +3,8 @@ package ru.autoqa.Java2021.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.autoqa.Java2021.addressbook.model.ContactData;
-import ru.autoqa.Java2021.addressbook.model.GroupData;
 
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 
 public class ContactModificationTests extends TestBase {
@@ -16,13 +14,13 @@ public class ContactModificationTests extends TestBase {
         if (! app.getContactHelper().isThereAContact()) {
             app.getContactHelper().createContact(new ContactData("Mary", "Brown", "rita", "Jn", "Petrozavodsk", "335999", "8923294455","dfdfn@gmail.com", "ssstr.ru", "6", "February", "1990", "-", "Test1","hi!"));
         }
-        app.getNavigationHelper().returnToHomePage();
+        app.goTo().returnToHomePage();
         List<ContactData> before = app.getContactHelper().getContactList();
         app.getContactHelper().editContact(before.size()-1);
         ContactData contact = new ContactData(before.get(before.size()-1).getId(),"Mary", "Mrown", "nicc", "Jn", "Petrozavodsk", "1111999", "8921194455","dfdfn@gmail.com", "ssstr.ru", "6", "February", "1990", "4", null, "hi!");
         app.getContactHelper().fillBaseInformation(contact, false);
         app.getContactHelper().submitContactModification();
-        app.getNavigationHelper().returnToHomePage();
+        app.goTo().returnToHomePage();
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size());
 
