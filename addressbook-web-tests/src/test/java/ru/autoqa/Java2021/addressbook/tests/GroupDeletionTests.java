3e99f8bd1535a8ec909674277b9c24f5e69feaa2
Natalia Shakh <me.nattee@gmail.com>
2021-02-26 20:26:21 +0300

    Лекция 5.2. Улучшение кода (группы)

--- addressbook-web-tests/src/test/java/ru/autoqa/Java2021/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/autoqa/Java2021/addressbook/tests/GroupDeletionTests.java
@@ -1,6 +1,7 @@
 package ru.autoqa.Java2021.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.autoqa.Java2021.addressbook.model.GroupData;
 
@@ -9,19 +10,22 @@ import java.util.List;
 
 public class GroupDeletionTests extends TestBase {
 
-  @Test
-  public void testGroupDeletion() {
+  @BeforeMethod
+  public void ensurePreconditions(){
     app.getNavigationHelper().gotoGroupPage();
     if (! app.getGroupHelper().isThereAGroup()){
       app.getGroupHelper().createGroup(new GroupData("Test1", null, null));
     }
+  }
+  @Test
+  public void testGroupDeletion() {
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() - 1);
-    app.getNavigationHelper().returnToHomePage();
+//    app.getNavigationHelper().returnToHomePage();
 
     before.remove(before.size() - 1);
     Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
