737cf4c46a6c917109df4993f502c292a9592316
Natalia Shakh <me.nattee@gmail.com>
2021-03-07 22:15:32 +0300

    Лекция 6.5. Загрузка тестовых данных из файла (группы и контакты) с ошибкой
    [Utils] [ERROR] [Error] java.lang.ArrayIndexOutOfBoundsException: 1

--- addressbook-web-tests/src/test/java/ru/autoqa/Java2021/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/autoqa/Java2021/addressbook/tests/ContactCreationTests.java
@@ -7,7 +7,10 @@ import ru.autoqa.Java2021.addressbook.model.ContactData;
 import ru.autoqa.Java2021.addressbook.model.Contacts;
 import ru.autoqa.Java2021.addressbook.model.GroupData;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -19,12 +22,16 @@ import static org.hamcrest.CoreMatchers.equalTo;
 public class ContactCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validContacts() {
+    public Iterator<Object[]> validContacts() throws IOException {
         List<Object[]> list = new ArrayList<Object[]>();
         File photo = new File("src/test/resources/stru.jpg");
-        list.add(new Object[]{new ContactData().withFirstname("Mary").withLastname("Brown").withNickname("mary").withPhoto(photo).withTitle("Jn").withAddress("Moscow, Green st. 15-87").withHomenumber("335999").withMobile("8923294455").withEmail("dfdfn@gmail.com").withEmail2("new_second@mail.com").withEmail3("new_3@mail.com").withHomepage("ssstr.ru").withBday("6").withBmonth("February").withByear("1990").withAday("-").withGroup("Test1").withNotes("hi!")});
-        list.add(new Object[]{new ContactData().withFirstname("Kate").withLastname("Green").withNickname("kate").withPhoto(photo).withTitle("Jn").withAddress("Moscow, Green st. 15-87").withHomenumber("335999").withMobile("8923294455").withEmail("dfdfn@gmail.com").withEmail2("new_second@mail.com").withEmail3("new_3@mail.com").withHomepage("ssstr.ru").withBday("6").withBmonth("February").withByear("1990").withAday("-").withGroup("Test1").withNotes("hi!")});
-        list.add(new Object[]{new ContactData().withFirstname("Bob").withLastname("Black").withNickname("bob").withPhoto(photo).withTitle("Jn").withAddress("Moscow, Green st. 15-87").withHomenumber("335999").withMobile("8923294455").withEmail("dfdfn@gmail.com").withEmail2("new_second@mail.com").withEmail3("new_3@mail.com").withHomepage("ssstr.ru").withBday("6").withBmonth("February").withByear("1990").withAday("-").withGroup("Test1").withNotes("hi!")});
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+        String line = reader.readLine();
+        while (line != null){
+            String[] split = line.split(";");
+            list.add(new Object[] {new ContactData().withFirstname(split[0]).withLastname(split[1]).withAddress(split[2])});
+            line = reader.readLine();
+        }
         return list.iterator();
     }
 
--- addressbook-web-tests/src/test/java/ru/autoqa/Java2021/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/autoqa/Java2021/addressbook/tests/GroupCreationTests.java
@@ -1,14 +1,13 @@
 package ru.autoqa.Java2021.addressbook.tests;
 
-import org.hamcrest.MatcherAssert;
 import org.testng.annotations.*;
 import ru.autoqa.Java2021.addressbook.model.GroupData;
 import ru.autoqa.Java2021.addressbook.model.Groups;
-
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Stack;
+
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -16,11 +15,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validGroups() {
+    public Iterator<Object[]> validGroups() throws IOException {
         List<Object[]> list = new ArrayList<Object[]>();
-        list.add(new Object[]{new GroupData().withName("test1").withHeader("header 1").withFooter("footer 1")});
-        list.add(new Object[]{new GroupData().withName("test2").withHeader("header 2").withFooter("footer 2")});
-        list.add(new Object[]{new GroupData().withName("test3").withHeader("header 3").withFooter("footer 3")});
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+        String line = reader.readLine();
+        while (line != null){
+            String[] split = line.split(";");
+            list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+            line = reader.readLine();
+        }
         return list.iterator();
     }
 
@@ -34,7 +37,7 @@ public class GroupCreationTests extends TestBase {
         assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
     }
 
-    @Test
+    @Test (enabled = false)
     public void testBadGroupCreation() throws Exception {
         app.goTo().groupPage();
         Groups before = app.group().all();
