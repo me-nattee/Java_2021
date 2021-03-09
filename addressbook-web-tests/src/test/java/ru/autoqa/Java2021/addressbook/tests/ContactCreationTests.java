3a532ffdf298b85db061dfc002b27f8275ab8708
Natalia Shakh <me.nattee@gmail.com>
2021-03-10 00:22:11 +0300

    6.9. Конфигурационные файлы для описания тестовых наборов (не доделано)

--- addressbook-web-tests/src/test/java/ru/autoqa/Java2021/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/autoqa/Java2021/addressbook/tests/ContactCreationTests.java
@@ -6,13 +6,11 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.autoqa.Java2021.addressbook.model.ContactData;
 import ru.autoqa.Java2021.addressbook.model.Contacts;
-import ru.autoqa.Java2021.addressbook.model.GroupData;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
