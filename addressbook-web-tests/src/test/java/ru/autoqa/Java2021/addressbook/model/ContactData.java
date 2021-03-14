050ebd60ab8a154c0436e8eb8f5da1c9412da6b0
Natalia Shakh <me.nattee@gmail.com>
2021-03-14 19:44:07 +0300

    Лекция 7.3. Условия отбора данных

--- addressbook-web-tests/src/test/java/ru/autoqa/Java2021/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/autoqa/Java2021/addressbook/model/ContactData.java
@@ -2,52 +2,105 @@ package ru.autoqa.Java2021.addressbook.model;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 import java.util.Objects;
 @XStreamAlias("contact")
+@Entity
+@Table(name="addressbook")
+
 
 public class ContactData {
     @XStreamOmitField
+    @Id
+    @Column(name="id")
     private int id = Integer.MAX_VALUE;
+
+    @Column(name="firstname")
     private String firstname;
+
+    @Column(name="lastname")
     private String lastname;
+
+    @Column(name="nickname")
     private String nickname;
+
+    @Column(name="title")
     private String title;
+
+    @Column(name="address")
+    @Type(type="text")
     private String address;
+
+    @Column(name="home")
+    @Type(type="text")
     private String homenumber;
+
+    @Column(name="mobile")
+    @Type(type="text")
     private String mobile;
+
+    @Column(name="email")
+    @Type(type="text")
     private String email;
+
+    @Column(name="email2")
+    @Type(type="text")
     private String email2;
+
+    @Column(name="email3")
+    @Type(type="text")
     private String email3;
+
+    @Column(name="homepage")
+    @Type(type="text")
     private String homepage;
+
+    @Transient
     private String bday;
+
+    @Transient
     private String bmonth;
+
+    @Transient
     private String byear;
+
+    @Transient
     private String aday;
+
+    @Column(name="notes")
+    @Type(type="text")
     private String notes;
+
+    @Transient
     private String group;
 
-    public File getPhoto() {
-        return photo;
-    }
-
-    public ContactData withPhoto(File photo) {
-        this.photo = photo;
-        return this;
-    }
-
+    @Transient
     private String allPhones;
+
+    @Transient
     private String allEmails;
-    private File photo;
+
+    @Column(name="photo")
+    @Type(type="text")
+    private String photo;
 
     public String getAllEmails() {
         return allEmails;
     }
 
-
     public String getAllPhones() {
         return allPhones;
+    }
+
+    public ContactData withPhoto(File photo) {
+        this.photo = photo.getPath();
+        return this;
+    }
+    public File getPhoto() {
+        return new File(photo);
     }
 
     public ContactData withAllPhones(String allPhones) {
