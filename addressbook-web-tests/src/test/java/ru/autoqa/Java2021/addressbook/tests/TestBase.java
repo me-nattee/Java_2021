1c8f1875c1093d827157fda616369c81612f9549
Natalia Shakh <me.nattee@gmail.com>
2021-03-15 22:07:01 +0300

    Лекция 7.5. (контакты и группы)

--- addressbook-web-tests/src/test/java/ru/autoqa/Java2021/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/autoqa/Java2021/addressbook/tests/TestBase.java
@@ -1,4 +1,6 @@
 package ru.autoqa.Java2021.addressbook.tests;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,10 +9,18 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.autoqa.Java2021.addressbook.appmanager.ApplicationManager;
+import ru.autoqa.Java2021.addressbook.model.ContactData;
+import ru.autoqa.Java2021.addressbook.model.Contacts;
+import ru.autoqa.Java2021.addressbook.model.GroupData;
+import ru.autoqa.Java2021.addressbook.model.Groups;
 
 import java.lang.reflect.Array;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
 
 public class TestBase {
 
@@ -37,6 +47,27 @@ public class TestBase {
     @AfterMethod (alwaysRun = true)
     public void logTestStop(Method m){
         logger.info("Stop test " + m.getName());
+    }
+
+    public void verifyGroupListInUI() {
+        if (Boolean.getBoolean("verifyUI")) {
+            Groups dbGroups = app.db().groups();
+            Groups uiGroups = app.group().all();
+            assertThat(uiGroups, equalTo(dbGroups.stream()
+                    .map((g) -> new GroupData().withId(g.getId()).withName(g.getName()))
+                    .collect(Collectors.toSet())));
+        }
+    }
+
+
+    public void verifyContactListInUI() {
+        if (Boolean.getBoolean("verifyUI")) {
+            Contacts dbContacts = app.db().contacts();
+            Contacts uiContacts = app.contact().all();
+            assertThat(uiContacts, equalTo(dbContacts.stream()
+                    .map((g) -> new ContactData().withId(g.getId()).withFirstname(g.getFirstname()))
+                    .collect(Collectors.toSet())));
+        }
     }
 
 }
